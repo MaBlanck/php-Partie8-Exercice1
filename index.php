77fa45fc8b283f814f50db59ee5a0cb67c4f94c6
Max <maxblanck@outlook.fr>
2020-12-10 16:35:31 +0100

    php Partie 8  Exercice 1

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Exercice 1 Partie 8</title>
+</head>
+
+<body>
+    <h1>Exercice 1 Partie 8</h1>
+    <p>Votre User Agent est: <?= $_SERVER['HTTP_USER_AGENT'] ?></p>
+    <p>Votre adresse IP est: <?= $_SERVER['REMOTE_ADDR'] ?></p>
+    <p>Le nom de votre serveur est: <?= $_SERVER['SERVER_NAME'] ?></p>
+</body>
+
+</html>
